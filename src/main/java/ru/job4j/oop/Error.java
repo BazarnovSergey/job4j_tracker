127943b3b58495fd6ba46f015eb20f9b9d5806ad
macbookpro <101sergey101@gmail.com>
2021-12-08 23:05:44 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Active: " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error memoryError = new Error(true, 1, "OutOfMemoryError");
+        Error fileError = new Error(true, 2, "FileNotFoundException");
+        Error arrayError = new Error(false, 3, "ArrayIndexOutOfBoundsException");
+        error.printInfo();
+        memoryError.printInfo();
+        fileError.printInfo();
+        arrayError.printInfo();
+    }
+}
