0688db0fa75efa603cc8c5abc437e674f689de28
macbookpro <101sergey101@gmail.com>
2021-12-09 22:48:55 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/oop/Calculator.java
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -1,36 +1,14 @@
 package ru.job4j.oop;
 
 public class Calculator {
-    private static int x = 5;
-
-    public static int sum(int y) {
-        return x + y;
+    public double add(double first, double second) {
+        return first + second;
     }
 
-    public int multiply(int a) {
-        return x * a;
-    }
-
-    public static int minus(int z) {
-        return z - x;
-    }
-
-    public int divide(int n) {
-        return n / x;
-    }
-
-    public int sumAllOperation(int w) {
-        return sum(w) + multiply(w) + minus(w) + divide(w);
-    }
-
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        int rsl = calculator.multiply(5);
-        System.out.println(rsl);
-        System.out.println(sum(6));
-        System.out.println(calculator.multiply(8));
-        System.out.println(minus(16));
-        System.out.println(calculator.divide(20));
-        System.out.println(calculator.sumAllOperation(10));
+    public double add(double first, double second, double third) {
+        return add(
+                first,
+                add(second, third)
+        );
     }
 }
