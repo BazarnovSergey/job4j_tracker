39060c899e6225de0f1fc230294ed6b5ccac5fbd
macbookpro <101sergey101@gmail.com>
2021-12-11 18:52:57 +0300

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -15,6 +15,12 @@ public class Computer {
         this.cpu = cpu;
     }
 
+    public Computer(boolean multiMonitor, double ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = (int) ssd;
+        this.cpu = cpu;
+    }
+
     public void printInfo() {
         System.out.println("Много мониторов: " + multiMonitor);
         System.out.println("SSD: " + ssd + " GB");
@@ -26,5 +32,7 @@ public class Computer {
         computer.printInfo();
         Computer comp = new Computer(true, 256, "AMD Ryzen 5 3600");
         comp.printInfo();
+        Computer forth = new Computer(true, 512.0, "AMD Ryzen 7 3700X");
+        forth.printInfo();
     }
 }
