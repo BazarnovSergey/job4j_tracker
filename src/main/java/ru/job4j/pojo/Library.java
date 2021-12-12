dcf2feeab25d541c365edf6c1ca6dcecc07cea21
macbookpro <101sergey101@gmail.com>
2021-12-12 22:47:58 +0300

    3. Массивы и модели. [#174892 #239185]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -1,16 +1,14 @@
 package ru.job4j.pojo;
 
+import java.util.Objects;
+
 public class Library {
     public static void main(String[] args) {
         Book cleanCode = new Book("Clean code", 356);
         Book goreOtUma = new Book("Gore Ot Uma", 98);
         Book voinaImir = new Book("Voina i Mir", 1274);
         Book headFirstJava = new Book("Head First Java", 722);
-        Book[] books = new Book[4];
-        books[0] = cleanCode;
-        books[1] = goreOtUma;
-        books[2] = voinaImir;
-        books[3] = headFirstJava;
+        Book[] books = {cleanCode, goreOtUma, voinaImir, headFirstJava};
         for (Book bk : books) {
             System.out.println(bk.getName() + " - " + bk.getCount());
         }
@@ -20,7 +18,7 @@ public class Library {
             System.out.println(bk.getName() + " - " + bk.getCount());
         }
         for (Book bk : books) {
-            if (bk.getName() == "Clean code") {
+            if (Objects.equals(bk.getName(), "Clean code")) {
                 System.out.println(bk.getName() + " - " + bk.getCount());
             }
         }
