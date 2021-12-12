7a6740d26c8c9decf432a57256a9f71700d43573
macbookpro <101sergey101@gmail.com>
2021-12-12 23:09:11 +0300

    refactoring class Store

--- src/main/java/ru/job4j/pojo/Store.java
+++ src/main/java/ru/job4j/pojo/Store.java
@@ -5,16 +5,12 @@ public class Store {
         Product milk = new Product("Milk", 10);
         Product bread = new Product("Bread", 4);
         Product egg = new Product("Egg", 19);
-        Product[] prods = new Product[3];
-        prods[0] = milk;
-        prods[1] = bread;
-        prods[2] = egg;
+        Product[] prods = {milk, bread, egg};
         for (Product pr : prods) {
             System.out.println(pr.getName() + " - " + pr.getCount());
         }
         System.out.println("Replace milk to oil.");
-        Product oil = new Product("Oil", 11);
-        prods[0] = oil;
+        prods[0] = new Product("Oil", 11);
         for (Product pr : prods) {
             System.out.println(pr.getName() + " - " + pr.getCount());
         }
