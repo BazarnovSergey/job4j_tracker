cea8afe12d759ded72a6a87b2319895103b265ce
Sergey <101sergey101@gmail.com>
2022-01-12 22:07:21 +0300

    1. Паспорт и жители [#212656 #249596]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.collection;
 
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -13,5 +14,14 @@ public class PassportOfficeTest {
         PassportOffice office = new PassportOffice();
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void notAdd() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizenDuplicate = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        Assert.assertFalse(office.add(citizenDuplicate));
     }
 }
